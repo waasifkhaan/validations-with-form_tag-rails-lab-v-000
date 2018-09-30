574f045fa6c10ae7a1ff7299a676ec53db9a06a7
Wasif Khan <waasif.khaan@gmail.com>
2018-09-30 18:05:45 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title , presence: true 
   validates :content , length: {maximum: 100}
-  validates :category , inclusion: {in: w%{Fiction Non-Fiction}}
+  validates :category , inclusion: {in: %w(Fiction Non-Fiction)}
 end
