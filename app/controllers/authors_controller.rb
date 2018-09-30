a282c3186a1fcb459c22cb3f03370cc72e9cdcb6
Wasif Khan <waasif.khaan@gmail.com>
2018-09-30 18:42:03 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,9 +9,13 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
-  end
+    if @author.valid?
+      @author.save
+      redirect_to author_path(@author)
+    else 
+      render :new
+    end
+  end 
 
   private
 
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name , presence: true 
+  validates_uniqueness_of :email 
+  validates :phone_number,  length: { is: 10 } 
 end
